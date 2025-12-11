c276e44f4a025fd0369cf5daf080b54ad7f113b1
Chan-Sol Lee <coldkid1124@gmail.com>
2025-12-11 09:04:02 +0900

    251211

--- vite-react/src/App.tsx
+++ vite-react/src/App.tsx
@@ -1,20 +1,15 @@
-// import { Header } from "./components/Header";
-// import { Main } from "./components/Main";
-// import { Footer } from "./components/Footer";
-
-import { useReducer } from 'react';
-import { counterReducer } from './reducer/counterReducer';
-
 export default function App() {
-    const [count, countDispatch] = useReducer(counterReducer, 0);
-
-    return (
-        <div>
-            <h1>Count: {count}</h1>
-
-            <button onClick={() => countDispatch({ type: 'DECREMENT' })}>감소</button>
-            <button onClick={() => countDispatch({ type: 'INCREMENT' })}>증가</button>
-            <button onClick={() => countDispatch({ type: 'RESET' })}>초기화</button>
-        </div>
-    );
+  return (
+    <>
+      <img src='/assets/images/ocean.jpg' alt='ocean' />
+      {/* <div className="bg"></div> */}
+      {/* <div
+        style={{
+          width: "640px",
+          height: "360px",
+          background: `url("/assets/images/ocean.jpg") no-repeat center center`,
+        }}
+      ></div> */}
+    </>
+  );
 }
